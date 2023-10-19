65569067579ee31f8f7080b39cce1db15354c663
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-19 16:30:36 +0800

    fix desktop view of hero banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import "../styles/hero.css"
-import hero from "../assets/images/mobile/image-hero.jpg"
+import heroMobile from "../assets/images/mobile/image-hero.jpg"
+import heroDesktop from "../assets/images/desktop/image-hero.jpg"
 
 function Hero() {
     return (
         <>
             <div className="hero">
-                <img src={hero} className="hero-img mobile" alt="hero" />
+                <img src={heroMobile} className="hero-img mobile" alt="hero-mobile" />                
+                <img src={heroDesktop} className="hero-img desktop" alt="hero-desktop" />
                 <div className="hero-heading">immersive experiences that deliver</div>
             </div>
         </>

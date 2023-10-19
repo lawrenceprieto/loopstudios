56b466aa45c77fddf77caf745b6accec71a72bc1
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-19 14:57:27 +0800

    import all the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
 import LandingPage from "./pages/LandingPage"
 
 function App() {
   return (
     <>
+      <Navbar />
       <LandingPage />
+      <Footer />
     </>
   )
 }
